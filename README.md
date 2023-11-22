fda5650bd9d399ba8362d7362940ab1bb954f495
dingjingmaster <dingjing@live.cn>
2023-11-22 10:12:44 +0800

    [完成] 镜像可引导，简单说明

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # graceful-linux-tools
+
+## 功能
+
+- [x] 制作可启动镜像
+- [x] 测试镜像可用否
+
+## 使用方法
+
+```shell
+# 制作可引导镜像
+./build -b
+
+# 测试可引导镜像
+./build -i ./work/graceful-linux-0.0.1.iso
+```
+
+## 依赖
+
+|依赖命令|依赖原因|
+|--------|--------|
+|`pacstrap`|用于生成根文件系统|
+|`chroot`|用于切换到根文件系统执行相关操作|
+|`mksquashfs`|用于将根文件系统制作成squashfs|
+|`sha512sum`|用于生成镜像的指纹，在ramfs中检测镜像是否被修改|
+|`xorriso`|用于生成可引导镜像|
+|`qemu`, `qemu-system-x86_64`|用于测试镜像是否可用|
--- build.sh
+++ build.sh
@@ -1,3 +1,75 @@
 #!/bin/bash
 
-$(dirname $(realpath -- $0))/bin/build-iso.sh
+workDir=$(dirname $(realpath -- $0))
+
+set -eu
+
+function print_help()
+{
+    local usagetext
+    IFS='' read -r -d '' usagetext <<EOF || true
+使用:
+    ./build.sh [参数]
+
+参数:
+    -b              开始构建 graceful-linux 镜像
+    -i [image]      引导镜像
+    -h              帮助
+
+例子:
+    构建镜像：
+        ./build.sh -b
+
+    测试构建镜像:
+        ./build.sh -i work/graceful-linux-0.0.1.iso
+EOF
+    printf '%s' "${usagetext}"
+}
+
+declare -x build image
+
+build=0
+image=""
+
+if (( ${#@} > 0 )); then
+    while getopts 'bhi:' flag; do
+        case "$flag" in
+            b)
+                build=1
+                ;;
+            h)
+                print_help
+                exit 0
+                ;;
+            i)
+                image="$OPTARG"
+                ;;
+            *)
+                print_help
+                exit 1
+                ;;
+        esac
+    done
+else
+    print_help
+    exit 1
+fi
+
+#echo $workDir
+#echo $image
+
+if [ $build -eq 1 ]; then
+    ${workDir}/bin/build-iso.sh
+    exit 0
+fi
+
+if [ "x$image" != "x" ]; then
+    ${workDir}/bin/launch-x86 -i ${image} || exit 0
+    exit 0
+fi
+
+print_help
+
+exit 0
+
+
